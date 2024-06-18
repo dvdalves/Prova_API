7a77bfd2889b7169180ff42ffa6c6a9fb720b590
David Alves <david.a0595@gmail.com>
2024-06-18 14:08:24 -0300

    Adicionando as migrations para os bancos SQL Server e SQLite

---
+++ Prova_API.Data/Migrations/SQLite/20240618170644_InitialMigrationForSQLite.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prova_API.Data.Migrations.SQLite
+{
+    /// <inheritdoc />
+    public partial class InitialMigrationForSQLite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: true),
+                    Valor = table.Column<decimal>(type: "TEXT", nullable: true),
+                    DataCadastro = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Ativo = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
